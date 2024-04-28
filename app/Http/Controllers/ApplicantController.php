4b28c2524909770b058360a4c17e0dc48667ad1b
walterarce <walterarce@outlook.com>
2024-04-28 19:22:03 -0300

    subo lo ejercitado en clase respecto a Eloquent y recuperar esos datos por medio de la vista y el controller

---
+++ app/Http/Controllers/ApplicantController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Applicant;
+use Illuminate\Http\Request;
+
+class ApplicantController extends Controller
+{
+    public function applicant()
+    {
+        $applicant = Applicant::all();
+//        dd($applicant);
+        return view('applicant',['applicants' => $applicant]);
+    }
+}

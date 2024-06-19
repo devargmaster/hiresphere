55668b218113c6e1be4c65932eff05a19af4bffe
walterarce <walterarce@outlook.com>
2024-06-19 14:19:50 -0300

    header para titulo de noticia en vista lleer

--- resources/views/news/show.blade.php
+++ resources/views/news/show.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
     <x-slot:title>{{ $new->title }}</x-slot:title>
     <div class="md:flex md:flex-col md:justify-center md:items-center text-center">
-        <p class="text-3xl font-bold mt-6 mb-4">{{ $new->title }}</p>
+        <h2 class="text-3xl font-bold mt-6 mb-4">{{ $new->title }}</h2>
         <div class="md:w-10/12 mb-6">
             <div class="md:flex md:flex-col md:items-center bg-white p-6 rounded-lg shadow-xl mb-4">
                 <img src="{{ asset('images/' . $new->image) }}" alt="Imagen Noticia" class="w-1/5 object-cover h-64 md:h-auto mb-4">
